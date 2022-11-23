87c1bd152c6e0cdd6cba55afadaf21b7553334db
Peprykos <pathan99@wp.pl>
2022-11-23 11:53:35 +0100

    po teście

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logowanie</title>
-</head>
-<body>
-    <div id="loginform">
-        <form action="" method="post">
-            <label for="loginID">Nazwa Użytkownika:</label><br>
-            <input type="text" name="login" id="loginID"><br>
-            <label for="loginID">Hasło:</label><br>
-            <input type="password" name="password" id="passwordID">
-        </form>
-    </div>
-</body>
-</html>
+<?php
+require_once('vendor/autoload.php');
+require_once('config.php');
+require_once('class/User.class.php');
+
+echo $twig->render('index', ['name' => 'Patryk']);
+
+//$user = new User('jkowalski', 'tajneHasło');
+/*
+if($user->register()) {
+    echo "Zarejestrowano poprawnie";
+} else {
+    echo "Błąd rejestracji użytkownika";
+}
+*/
+
+/*if($user->login()) {
+    echo "Zalogowano poprawnie";
+} else {
+    echo "Błędny login lub hasło";
+}
+*/
+?>
